853f5a8fd45e35f01daedcd341329510a5f7e78e
Alessandro Paredes <32472350+alessparedes@users.noreply.github.com>
2022-04-20 00:03:08 -0600

    Create BaseBall_Game.cs
    Solution

---
+++ BaseBall_Game.cs
@@ -0,0 +1,68 @@
+/*
+You are keeping score for a baseball game with strange rules. The game consists of several rounds, where the scores of past rounds may affect future rounds' scores.
+
+At the beginning of the game, you start with an empty record. You are given a list of strings ops, where ops[i] is the ith operation you must apply to the record and is one of the following:
+
+An integer x - Record a new score of x.
+"+" - Record a new score that is the sum of the previous two scores. It is guaranteed there will always be two previous scores.
+"D" - Record a new score that is double the previous score. It is guaranteed there will always be a previous score.
+"C" - Invalidate the previous score, removing it from the record. It is guaranteed there will always be a previous score.
+Return the sum of all the scores on the record.
+
+ Input: ops = ["5","2","C","D","+"]
+Output: 30
+
+Example 1:
+Input: ["5","2","C","D","+"]
+Output: 30
+Explanation:
+Round 1: You could get 5 points. The sum is: 5.
+Round 2: You could get 2 points. The sum is: 7.
+Operation 1: The round 2's data was invalid. The sum is: 5.
+Round 3: You could get 10 points (the round 2's data has been removed). The sum is: 15.
+Round 4: You could get 5 + 10 = 15 points. The sum is: 30.
+Example 2:
+Input: ["5","-2","4","C","D","9","+","+"]
+Output: 27
+Explanation:
+Round 1: You could get 5 points. The sum is: 5.
+Round 2: You could get -2 points. The sum is: 3.
+Round 3: You could get 4 points. The sum is: 7.
+Operation 1: The round 3's data is invalid. The sum is: 3.
+Round 4: You could get -4 points (the round 3's data has been removed). The sum is: -1.
+Round 5: You could get 9 points. The sum is: 8.
+Round 6: You could get -4 + 9 = 5 points. The sum is 13.
+Round 7: You could get 9 + 5 = 14 points. The sum is 27.
+Note:
+The size of the input list will be between 1 and 1000.
+Every integer represented in the list will be between -30000 and 30000.
+ */
+ 
+ static int count(string[] ops)
+	{
+		
+		Stack<int> stack = new Stack<int>();
+        
+        foreach(string data in ops)
+        {
+            if (data.Equals("+"))
+            {
+            	int top = stack.Pop();
+              	int newtop = top + stack.Peek();
+                stack.Push(top);
+                stack.Push(newtop);
+            }else if (data.Equals("C"))
+            {
+            	stack.Pop();
+            }else if (data.Equals("D"))
+            {
+            	stack.Push(2*stack.Peek());
+            } else 
+            {
+            	stack.Push(int.Parse(data));
+            }
+        }
+      int ans=0;
+      foreach(int score in stack) ans += score;
+      return ans;
+	}
