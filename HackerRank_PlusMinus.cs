4d7a60c924dfc0520fc5bb2b8d464085407295b3
Alessandro Paredes <32472350+alessparedes@users.noreply.github.com>
2023-04-07 02:05:17 -0600

    Update HackerRank_PlusMinus.cs
    I think that to display the result it could be changed to something optimal, but if someone knows, please indicate it.

--- HackerRank_PlusMinus.cs
+++ HackerRank_PlusMinus.cs
@@ -1,3 +1,4 @@
+//Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with places after the decimal. The test cases are scaled to six decimal places, though answers with absolute error of up to are acceptable. Example There are elements, two positive, two ne…
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Collections;
