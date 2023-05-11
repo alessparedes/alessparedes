b2465f8457b51f76624318ed3bdf73afa38d46be
Christian Alessandro Paredes <32472350+alessparedes@users.noreply.github.com>
2023-05-11 15:42:18 -0400

    Update CodeSignal_Demo.cs
    Next requirement of theCodeSignal demo

--- CodeSignal_Demo.cs
+++ CodeSignal_Demo.cs
@@ -1,8 +1,9 @@
 /*
 Your task is to implement a simple container of integer numbers. As a first step, consider implementing the following two operations:
 
-ADD <value> should add the specified integer value to the container. Returns an empty string.
-EXISTS <value> should check whether the specific integer value exists in the container. Returns "true" if the value exists, "false" otherwise.
+- ADD <value> should add the specified integer value to the container. Returns an empty string.
+- EXISTS <value> should check whether the specific integer value exists in the container. Returns "true" if the value exists, "false" otherwise.
+- REMOVE <value> should remove the specified integer value to the container. Returns "true" if the value exists, "false" otherwise.
 The container is supposed to be empty at the beginning of execution.
 Given a list of queries, return the list of answers for these queries. To pass to the next level you have to pass all tests at this level.
 #Example
@@ -25,19 +26,32 @@ the output should be solution(queries) = ["", "", "", "", "true", "true", "true"
 
 */
 string[] solution(string[][] queries) {
-    int queriesLength = queries.Length;
-    string[] result = new string[queriesLength];
-    string[] stringAdd =  new string[queriesLength];
+    string[] result = new string[queries.Length];
+    string[] stringAdd =  new string[queries.Length];
+    string[] newResult =  new string[queries.Length];
     
-    for (int i=0; i<queriesLength; i++){
-            if (queries[i][0].ToString().ToUpper() == "ADD"){
-                stringAdd[i] = queries[i][1].ToString();
-                result[i] = "";
-            }
-            if (queries[i][0].ToString().ToUpper() == "EXISTS"){
-                result[i] = Array.Exists(stringAdd, x => x == queries[i][1].ToString()).ToString().ToLower();
-            }
+    for (int i=0; i<queries.Length; i++){
+        if (queries[i][0].ToString().ToUpper() == "ADD"){
+            stringAdd[i] = queries[i][1].ToString();
+            result[i] = "";
+        }
+        if (queries[i][0].ToString().ToUpper() == "EXISTS"){
+            /* No lo pude probar
+            if (Array.Exists(queries, x => x == "REMOVE")) {
+                newResult[i] = Array.Exists(stringAdd, x => x == queries[i][1].ToString()).ToString().ToLower();
+            } else {
+                result[i] = Array.Exists(stringAdd, x => x == queries[i][1].ToString()).ToString().ToLower();    
+            }*/
+            result[i] = Array.Exists(stringAdd, x => x == queries[i][1].ToString()).ToString().ToLower();    
+        }
+        if (queries[i][0].ToString().ToUpper() == "REMOVE"){
+            newResult = result.Where(x=> x != queries[i][1].ToString()).ToArray();
+            newResult[i] = Array.Exists(stringAdd, x => x == queries[i][1].ToString()).ToString().ToLower();
+            
+            Console.WriteLine(newResult[i] + " -> " +queries[i][1].ToString());
+            return newResult;
+        }
     }
-    //Console.WriteLine(queries[1][0].ToString());
     return result;
 }
+
